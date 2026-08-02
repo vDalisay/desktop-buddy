145744bf5c2bedb3e8d70d4ffbd9ebe00190c360
vDalisay <andersenvincentr@gmail.com>
2026-08-02 20:18:00 +0200

    fix(a6): import app scenario types

--- src/Testing/CharacterSwapPhysicsInvariantPreparedScenario.cs
+++ src/Testing/CharacterSwapPhysicsInvariantPreparedScenario.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using DesktopBuddy.App;
 using DesktopBuddy.Buddy.Physics;
 using DesktopBuddy.Buddy.Presentation3D;
 using DesktopBuddy.Domain.Persistence;
