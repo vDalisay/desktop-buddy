d0784370af88d1595baa49fb837362b7c16b60fe
vDalisay <andersenvincentr@gmail.com>
2026-08-02 19:25:43 +0200

    wip(a4): add exact character render keys

---
+++ src/Buddy/Presentation3D/Characters/CharacterRenderKeys.cs
@@ -0,0 +1,57 @@
+using System;
+using DesktopBuddy.Domain.Characters;
+using DesktopBuddy.Domain.Presentation;
+using Godot;
+
+namespace DesktopBuddy.Buddy.Presentation3D.Characters;
+
+public readonly record struct FaceRenderKey(
+    CompiledFeatureAppearance Eyes,
+    CompiledFeatureAppearance Brows,
+    CompiledFeatureAppearance Mouth,
+    FaceRenderState State,
+    Color TrustedOutlineColor);
+
+public readonly record struct AccentRenderKey(
+    CompiledFeatureAppearance Accent,
+    Color TrustedOutlineColor);
+
+public static class BuiltInCharacterAppearance
+{
+    private static readonly Rgba32 FeatureColor = Rgba32.Parse("#183042");
+
+    public static CompiledCharacterAppearance Value { get; } = new(
+        Guid.Empty,
+        new PartColorSet(
+            CharacterPartColors.BuiltInHead,
+            CharacterPartColors.BuiltInTorso,
+            CharacterPartColors.BuiltInHand,
+            CharacterPartColors.BuiltInHand,
+            CharacterPartColors.BuiltInFoot,
+            CharacterPartColors.BuiltInFoot),
+        new CompiledFeatureAppearance(
+            CharacterFeatureIds.EyesSoftOval,
+            NormalizedFeatureTransform.Identity,
+            FeatureColor),
+        new CompiledFeatureAppearance(
+            CharacterFeatureIds.BrowsSoftArc,
+            NormalizedFeatureTransform.Identity,
+            FeatureColor),
+        new CompiledFeatureAppearance(
+            CharacterFeatureIds.MouthRounded,
+            NormalizedFeatureTransform.Identity,
+            FeatureColor),
+        new CompiledFeatureAppearance(
+            CharacterFeatureIds.AccentNone,
+            NormalizedFeatureTransform.Identity,
+            FeatureColor));
+
+    public static FaceRenderState NeutralFaceState { get; } = FaceComposer.Compose(
+        FaceExpressionCatalog.Resolve(":|"),
+        blinkClosed: false,
+        chewActive: false,
+        chewFrame: 0,
+        faceSuppressed: false,
+        pupilX: 0.0f,
+        pupilY: 0.0f);
+}
