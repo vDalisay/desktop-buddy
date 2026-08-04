1723b1800cf538af9d65df50e303b3f42dd70b8d
vDalisay <andersenvincentr@gmail.com>
2026-08-04 22:36:33 +0200

    test(phase-b): register B6 closure scenarios

--- src/Testing/PaintingPhaseBScenarioRegistration.cs
+++ src/Testing/PaintingPhaseBScenarioRegistration.cs
@@ -28,7 +28,9 @@ internal static class PaintingPhaseBScenarioRegistration
         factories["paint_memory_budget"] = () => new PaintMemoryBudgetScenario();
         factories["paint_persistence_roundtrip"] = () => new PaintPersistenceRoundtripScenario();
         factories["paint_invalid_png_rejected"] = () => new PaintInvalidPngRejectedScenario();
+        factories["paint_save_failure_preserves_working_copy"] = () => new PaintSaveFailurePreservesWorkingCopyScenario();
         factories["paint_preview_has_no_physics"] = () => new PaintPreviewHasNoPhysicsScenario();
+        factories["paint_runtime_fidelity"] = () => new PaintRuntimeFidelityScenario();
         factories["paint_under_expression_layer_order"] = () => new PaintLayerOrderScenario();
     }
 }
