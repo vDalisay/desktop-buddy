5da33cae91f877ca4c978ed5ea95c2f8c463e5da
vDalisay <andersenvincentr@gmail.com>
2026-08-18 00:08:27 +0200

    Register boxing glove critical-head capture scenario

--- src/Testing/ScenarioCatalog.cs
+++ src/Testing/ScenarioCatalog.cs
@@ -44,6 +44,7 @@ public static class ScenarioCatalog
         ["pet_tickle_mood"] = () => new PetTickleMoodScenario(),
         ["m3_presentation"] = () => new M3PresentationScenario(),
         ["tool_feel_reactions"] = () => new ToolFeelReactionScenario(),
+        ["glove_critical_head_impact"] = () => new GloveCriticalHeadImpactScenario(),
         ["presentation_3d"] = () => new Presentation3DScenario(),
         ["presentation_look"] = () => new PresentationLookScenario(),
         ["character_rig_view"] = () => new CharacterRigViewScenario(),
