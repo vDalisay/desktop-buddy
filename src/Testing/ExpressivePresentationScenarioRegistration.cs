1d0a9b51f77c0b93cd35d5e4c88b25ca900b8a20
vDalisay <andersenvincentr@gmail.com>
2026-09-07 01:06:52 +0200

    Fix expressive scenario registration

--- src/Testing/ExpressivePresentationScenarioRegistration.cs
+++ src/Testing/ExpressivePresentationScenarioRegistration.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 namespace DesktopBuddy.Testing;
@@ -6,8 +9,14 @@ namespace DesktopBuddy.Testing;
 internal static class ExpressivePresentationScenarioRegistration
 {
     [ModuleInitializer]
-    internal static void Register() =>
-        PhaseACharacterScenarioCatalog.Register(
-            "expressive_presentation",
-            static () => new ExpressivePresentationScenario());
+    internal static void Register()
+    {
+        FieldInfo field = typeof(ScenarioCatalog).GetField(
+            "Factories",
+            BindingFlags.NonPublic | BindingFlags.Static)
+            ?? throw new InvalidOperationException("Scenario registry field was not found.");
+        var factories = (Dictionary<string, Func<IScenario>>?)field.GetValue(null)
+            ?? throw new InvalidOperationException("Scenario registry was not initialized.");
+        factories["expressive_presentation"] = () => new ExpressivePresentationScenario();
+    }
 }
