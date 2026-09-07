87e248fdd42ddf4d2e233d4c23ac59489191e319
vDalisay <andersenvincentr@gmail.com>
2026-09-07 12:20:07 +0200

    Remove obsolete runtime achievement namespace

--- tests/DesktopBuddy.Domain.Tests/Persistence/ProgressResetTests.cs
+++ tests/DesktopBuddy.Domain.Tests/Persistence/ProgressResetTests.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using DesktopBuddy.Achievements;
 using DesktopBuddy.App;
 using DesktopBuddy.Domain.Achievements;
 using DesktopBuddy.Domain.Autonomy;
