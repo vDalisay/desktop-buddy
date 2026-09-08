202f13f9f8937523eb8f981904920556a4991d2f
vDalisay <andersenvincentr@gmail.com>
2026-09-08 18:41:35 +0200

    fix(persistence): import canonical scene position

--- src/Persistence/SceneProgressBootstrapCoordinator.cs
+++ src/Persistence/SceneProgressBootstrapCoordinator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using DesktopBuddy.Domain.Environment;
 using DesktopBuddy.Domain.Persistence;
 using DesktopBuddy.Domain.Platform;
 using DesktopBuddy.Domain.Scenes;
