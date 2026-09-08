dcc23383a7e04e3a1e27b27e5226be295e3001e0
vDalisay <andersenvincentr@gmail.com>
2026-09-08 22:26:06 +0200

    fix(environment): import legacy progress state

--- src/Persistence/IEnvironmentProgressPersistence.cs
+++ src/Persistence/IEnvironmentProgressPersistence.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using DesktopBuddy.Domain.Environment;
+using DesktopBuddy.Domain.Persistence;
 using DesktopBuddy.Domain.Scenes;
 
 namespace DesktopBuddy.Persistence;
